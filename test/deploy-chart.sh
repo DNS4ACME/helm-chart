d54c56fc540502c4e6e16afb1e933050989e069d
bitmotivator <213508235+bitmotivator@users.noreply.github.com>
2025-08-13 07:25:16 +0200

    Fixed test script

--- test/deploy-chart.sh
+++ test/deploy-chart.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
-cd $SCRIPT_DIR/../dns4cme
+cd $SCRIPT_DIR/../dns4acme
 helm install dns4acme --namespace dns4acme . --values $SCRIPT_DIR/values.yaml
 helm test example.com
