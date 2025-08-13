2bb482394555dfe1cd79100792d6e850ea23f693
bitmotivator <213508235+bitmotivator@users.noreply.github.com>
2025-08-13 07:39:46 +0200

    Debug messages and fixes

--- test/deploy-chart.sh
+++ test/deploy-chart.sh
@@ -5,5 +5,8 @@ set -euo pipefail
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
 cd $SCRIPT_DIR/../dns4acme
+echo "Running helm install..."
 helm install dns4acme --namespace dns4acme . --values $SCRIPT_DIR/values.yaml
-helm test --namespace dns4acme example.com
+
+echo "Running helm test..."
+helm test --namespace dns4acme dns4acme
